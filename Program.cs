6e70a16c0ededd13d8c5ea866b0b8b7891408109
İsmail Kurt <ismail.kurt7749@gmail.com>
2023-08-22 12:56:42 +0300

    Dizi Metotları

--- Program.cs
+++ Program.cs
@@ -4,32 +4,45 @@ namespace test;
 class Program
 {
     static void Main(string[] args)
-    {
-        //string[] isimler = new string [5];
+    {   
+        int[] numaralar = {5,3,6,1,2};
+
+        // isimler [0] = "Ali";
+        //isimler.SetValue("Ali",0);
+
+        // System.Console.WriteLine(isimler.GetValue(0));
+        // System.Console.WriteLine(Array.IndexOf(isimler,"Çınar"));
+        // System.Console.WriteLine(isimler.Length);
+
+        //Array.Sort(isimler);
+        //Array.Sort(numaralar);
         
+        //Array.Reverse(isimler);
+        //Array.Reverse(numaralar);
+
+        //Array.Clear(isimler,0,2);
+        //Array.Clear(numaralar,0,2);
+
+        // System.Console.WriteLine(isimler.GetValue(0));
+        // System.Console.WriteLine(numaralar.GetValue(0));
+
+        // System.Console.WriteLine(isimler[0]);
+        // System.Console.WriteLine(isimler[^2]);
+
         string[] isimler = {"Ahmet","Çınar","Ada","Yiğit","Sena"};
 
-        // isimler [0] = "Ahmet";
-        // isimler [1] = "Çınar";
-        // isimler [2] = "Ada";
-        // isimler [3] = "Yiğit";
-        // isimler [4] = "Sena";
+        // var result = isimler [1..4];
+        // var result = isimler [1..];
+        var result = isimler [..3];
 
-        // int [] numaralar = new int [5];
 
-        int [] numaralar = {101,102,103,104,105};
+        foreach (var isim in result)
+        {
+            System.Console.WriteLine(isim);
+        }
 
-        // numaralar [0] = 101;
-        // numaralar [1] = 102;
-        // numaralar [2] = 103;
-        // numaralar [3] = 104;
-        // numaralar [4] = 105;
-    
-    System.Console.WriteLine($"öğrenci adı: {isimler [0]} öğrenci numarası: {numaralar [0]}");
-    System.Console.WriteLine($"öğrenci adı: {isimler [1]} öğrenci numarası: {numaralar [1]}");
-    System.Console.WriteLine($"öğrenci adı: {isimler [2]} öğrenci numarası: {numaralar [2]}");
-    System.Console.WriteLine($"öğrenci adı: {isimler [3]} öğrenci numarası: {numaralar [3]}");
-    System.Console.WriteLine($"öğrenci adı: {isimler [4]} öğrenci numarası: {numaralar [4]}");
+        string msg = "Hello there";
+        System.Console.WriteLine(msg[0..5]);
 
     }
 }
